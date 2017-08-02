efe1dc84779f68fc23b123f7ce3706531a21352b
Rishi Karri <rishikarri1@gmail.com>
2017-08-02 17:12:34 -0400

    understand why it is wrapped in so many functions

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -11,18 +11,26 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests - all of the tests in teh app section passed or failed etc. 
 // in practice, give it the name of the component we are testing
-describe('App'){
+describe('App', () => {
 
-}
+	it('shows the correct text', () => {
+		// use expect to make an assertion about a target
+		
+	})
+})
+
+// this is a core part about how mocha runs test. the tests are not immediately run, if this function throws an error, don't worry about it, i am a testing suite
+// the purpose of writing it like this is so that mocha can safely execute our code without worrying about some error being thrown 
+
 
 // use "it" to test a single attribute of a target - we use it to group one single test - i am testing something very specific
 //here is exactly what I'm testing about app
 // the string is used only for the report - solely direction to us, the developers on what the block is used for s
 
-it('shows the correct text')
+
 
 // use 'expect' to make an assertion about a target 
 // going to use this these through all of our tests
 // from testing framework to testing framework, they all carry describe, it and expect - we are learning mocha but they all use the same topics - knowledge carries over to the other testing frameworks as well
 // used to test a very particular attribute about our target, our app component
-expect
+
